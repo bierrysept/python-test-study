4ca16330c5b45232de464a1419d424f805fce4f5
Bierrysept <bierrysept@gmail.com>
2023-11-19 21:12:42 +0300

    pytest lesson 5

--- src/calculator.py
+++ src/calculator.py
@@ -2,6 +2,8 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
     
     def add(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,23 +1,30 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
+
 from src.calculator import Calculator
 from typing import Union
 
-@pytest.mark.parametrize(
-        "x, y, res",
-        [
-            (1, 2, 0.5),
-            (5, -1, -5)
-        ]
-        )
-def test_divide(x: Union[int, float], y: Union[int, float], res: Union[int, float]):
-    assert Calculator().divide(x, y) == res
+class TestCalculator:
+    @pytest.mark.parametrize(
+            "x, y, res, expectation",
+            [
+                (1, 2, 0.5, does_not_raise()),
+                (5, -1, -5, does_not_raise()),
+                (5, "-1", -5, pytest.raises(TypeError)),
+            ]
+            )
+    def test_divide(self, x: Union[int, float], y: Union[int, float], res: Union[int, float], expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
-@pytest.mark.parametrize(
-        "x, y, res",
-        [
-            (1, 2, 3),
-            (5, -1, 4)
-        ]
-        )
-def test_add(x: Union[int, float], y: Union[int, float], res: Union[int, float]):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+            "x, y, res, expectation",
+            [
+                (1, 2, 3, does_not_raise()),
+                (5, -1, 4, does_not_raise()),
+                (5, "-1", -5, pytest.raises(TypeError)),
+            ]
+            )
+    def test_add(self, x: Union[int, float], y: Union[int, float], res: Union[int, float], expectation):
+        with expectation:   
+            assert Calculator().add(x, y) == res
